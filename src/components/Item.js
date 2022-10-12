be64ecc3ac661d563baf16ffc00c4fa22cd8a6b2
Daria Approvato <dariaapprovato@gmail.com>
2022-10-12 13:44:54 -0400

    commit tree c82f0e0fe49011a59f5fc0fde38e5713395791bd

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React , { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false)
+
+  const itemClass = isAdded ? "in-cart" : ""
+
+  function handleClick() {
+    setIsAdded((isAdded) => !isAdded)
+  }
+
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className= {isAdded ? "remove" : "add"} onClick={handleClick}> {isAdded ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
